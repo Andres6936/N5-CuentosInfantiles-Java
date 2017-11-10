3ea32f3e83366e28c763b9514890e77e9bc40884
Joan Andrés <andres6936@live.com>
2017-11-10 08:30:58 -0500

    Add Run.cmd

---
+++ bin/win/Run.cmd
@@ -0,0 +1,21 @@
+@echo off
+
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+REM Universidad Santiago de Cali (Santiago de Cali - Colombia)
+REM Departamento de Ingeniería de Sistemas y Computación
+REM Licenciado bajo el esquema Academic Free License version 2.1
+REM
+REM Proyecto Cupi2 (http://cupi2.uniandes.edu.co)
+REM Ejercicio: N5-CuentosInfantiles-Java
+REM Autor: Joan Andrés Buriticá Salazar
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+SET CLASSPATH=
+
+REM ---------------------------------------------------------
+REM Ejecucion del programa
+REM ---------------------------------------------------------
+
+cd ../..
+java -ea -classpath ./lib/cuentos Infantiles.jar edu.jabs.cuentosInfantiles.interfaz.InterfazCuentosInfantiles
+cd bin/win
