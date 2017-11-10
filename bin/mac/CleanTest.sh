d9aa301852874310996431bf04bb1989d59dfc16
Joan Andrés <andres6936@live.com>
2017-11-09 23:40:33 -0500

    Add CleanTest.sh

---
+++ bin/mac/CleanTest.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Universidad Santiago de Cali (Santiago de Cali - Colombia)
+# Departamento de Ingeniería de Sistemas y Computación
+# Licenciado bajo el esquema Academic Free License version 2.1
+#
+# Proyecto Cupi2 (http://cupi2.uniandes.edu.co)
+# Ejercicio: N5-CuentosInfantiles-Java
+# Autor: Joan Andrés Buriticá Salazar
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+stty -echo
+
+cd ../../test
+rm -rf classes/*
+rm -rf lib/cuentos InfantilesTest.jar
+
+cd ../bin/mac
