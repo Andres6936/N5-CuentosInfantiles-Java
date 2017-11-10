78678e2bc46969edfe596c93e8adf7bdcb468900
Joan Andrés <andres6936@live.com>
2017-11-10 08:30:37 -0500

    Add CleanTest.cmd

---
+++ bin/win/CleanTest.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+REM Universidad Santiago de Cali (Santiago de Cali - Colombia)
+REM Departamento de Ingeniería de Sistemas y Computación
+REM Licenciado bajo el esquema Academic Free License version 2.1
+REM
+REM Proyecto Cupi2 (http://cupi2.uniandes.edu.co)
+REM Ejercicio: N5-CuentosInfantiles-Java
+REM Autor: Joan Andrés Buriticá Salazar
+REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+SET CLASSPATH=
+
+cd ../../test
+rm -rf classes/*
+rm -rf lib/cuentos InfantilesTest.jar
+
+cd ../bin/win
