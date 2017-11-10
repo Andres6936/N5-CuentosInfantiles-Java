eb181a9c7b78944d03406ce381ee674fd482cbee
Joan Andrés <andres6936@live.com>
2017-11-09 22:37:11 -0500

    Update PanelNavegacion.java

--- src/edu/jabs/cuentosInfantiles/interfaz/PanelNavegacion.java
+++ src/edu/jabs/cuentosInfantiles/interfaz/PanelNavegacion.java
@@ -1,5 +1,51 @@
 package edu.jabs.cuentosInfantiles.interfaz;
 
-public class PanelNavegacion
-{
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+
+public class PanelNavegacion extends JPanel{
+
+    // Constantes
+
+    // Atributos
+
+    InterfazCuentosInfantiles principal;
+
+    private JButton butAtrasA;
+    private JButton butAtrasTotal;
+    private JButton butComprar;
+    private JButton butAdelanteTotal;
+    private JButton butAdelanteA;
+
+    // Constructor
+
+    public PanelNavegacion(InterfazCuentosInfantiles interfazCuentosInfantiles)
+    {
+        principal = interfazCuentosInfantiles;
+
+        setLayout(new GridLayout(1, 5));
+        TitledBorder border = BorderFactory.createTitledBorder("Navegación");
+        border.setTitleColor(Color.BLUE);
+        setBorder(border);
+
+        butAtrasA = new JButton("<");
+        butAtrasTotal = new JButton("<<");
+        butComprar = new JButton("Comprar");
+        butAdelanteTotal = new JButton(">>");
+        butAdelanteA = new JButton(">");
+
+        add(butAtrasA);
+        add(butAtrasTotal);
+        add(butComprar);
+        add(butAdelanteTotal);
+        add(butAdelanteA);
+    }
+
+    // Métodos
+
 }
